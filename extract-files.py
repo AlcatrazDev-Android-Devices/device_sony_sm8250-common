8ccc1d3ec03639fe0dc4481390fe3227afd6caf4
kyasu <kyasu.dev@gmail.com>
2025-03-17 10:02:30 +0900

    sm8250-common: Enable ELF checks for {libwfdaac_vendor,libc2dcolorconvert,libril} dependent libs
    Change-Id: Ie43980c52df02a767d67f644755f886781711452

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -64,9 +63,6 @@ lib_fixups: lib_fixups_user_type = {
     (
         'libOmxCore',
         'libplatformconfig',
-        'libwfdaac_vendor',
-        'libc2dcolorconvert',
-        'libril',
     ): lib_fixup_remove,
 }
 
